cceafc9b34175721663b32eb3f5f5774ffe2f9b1
Matheus Ryan <mrysn@ic.ufal.br>
2025-04-11 10:46:49 -0300

    [FIX]: Reduces the number of ports

--- main.asm
+++ main.asm
@@ -2,16 +2,17 @@
 ; Relógio digital com display de 7 segmentos para minutos e segundos
 ; Autores: Matheus Ryan, Lucas Heron, Rafael Luciano
 ; Data: 07/04/2025
-;
 
 
 ; ===================== CONSTANTES DE HARDWARE =====================
-.equ PORT_MINUTOS = PORTB       ; PB0-PB3: Minutos dezenas, PB4-PB7: Minutos unidades
-.equ PORT_SEGUNDOS = PORTD      ; PD0-PD3: Segundos dezenas, PD4-PD7: Segundos unidades
+.equ PORT_BCD      = PORTB       ; PB0-PB3: Linhas de BCD para os 4 displays
+.equ DDR_BCD       = DDRB
+.equ PORT_CTRL     = PORTD       ; PD0-PD3: Controle de cada display (1 por vez)
+.equ DDR_CTRL      = DDRD
 
 ; ===================== CONSTANTES DE TEMPORIZAÇÃO =================
-.equ VALOR_INICIAL_TIMER = 256 - (16000000/1024/100)  ; 100 interrupções/segundo
-.equ NUMERO_OVERFLOWS = 100       ; 100 interrupções = 1 segundo
+.equ VALOR_INICIAL_TIMER = 256 - (160)  ; 100 interrupções/segundo
+.equ NUMERO_OVERFLOWS    = 100          ; 100 interrupções = 1 segundo
 
 ; ===================== NOMES DE REGISTRADORES =====================
 .def reg_temp      = r16
@@ -19,6 +20,7 @@
 .def reg_unidades  = r18
 .def reg_aux       = r19
 .def reg_status    = r20
+.def reg_display   = r21
 
 ; ===================== ALOCAÇÃO DE VARIÁVEIS ======================
 .dseg
@@ -44,28 +46,28 @@ inicio:
     out SPL, reg_temp
 
     ; Configura portas como saída
-    ldi reg_temp, 0xFF
-    out DDRB, reg_temp
-    out DDRD, reg_temp
+    ldi reg_temp, 0x0F
+    out DDR_BCD, reg_temp        ; PB0-PB3 como saída (BCD)
+    out DDR_CTRL, reg_temp       ; PD0-PD3 como saída (controle de displays)
 
     ; Zera contadores
     clr reg_temp
     sts segundos, reg_temp
     sts minutos, reg_temp
     sts contador_overflow, reg_temp
-    call atualiza_displays
 
     ; Configura Timer0
-    ldi reg_temp, (1<<CS02)|(1<<CS00)
+    ldi reg_temp, (1<<CS02)|(1<<CS00)  ; Prescaler de 1024
     out TCCR0B, reg_temp
     ldi reg_temp, VALOR_INICIAL_TIMER
     out TCNT0, reg_temp
-    ldi reg_temp, (1<<TOIE0)
+    ldi reg_temp, (1<<TOIE0)           ; Habilita interrupção por overflow
     sts TIMSK0, reg_temp
 
     sei  ; Habilita interrupções globais
 
 loop_principal:
+    rcall atualiza_displays
     rjmp loop_principal
 
 ; ================= ROTINA: DIVISÃO POR 10 ========================
@@ -86,34 +88,85 @@ div_pronto:
     pop reg_aux
     ret
 
-; =============== ATUALIZAÇÃO DOS DISPLAYS ===============
+; =============== ATUALIZAÇÃO DOS DISPLAYS ======================
 atualiza_displays:
     push reg_temp
     push reg_dezenas
     push reg_unidades
     push reg_aux
+    push reg_display
+    push r22
+    push r23
+    push r24
+    push r25
 
-    ; ----- Minutos -----
-    lds reg_temp, minutos
-    rcall dividir_por_10
-    swap reg_unidades
-    or reg_unidades, reg_dezenas
-    out PORT_MINUTOS, reg_unidades
-
-    ; ----- Segundos -----
+    ; ------ Lê minutos e segundos uma vez ------
     lds reg_temp, segundos
     rcall dividir_por_10
-    swap reg_unidades
-    or reg_unidades, reg_dezenas
-    out PORT_SEGUNDOS, reg_unidades
+    mov r22, reg_unidades     ; Segundos unidade
+    mov r23, reg_dezenas      ; Segundos dezena
 
+    lds reg_temp, minutos
+    rcall dividir_por_10
+    mov r24, reg_unidades     ; Minutos unidade
+    mov r25, reg_dezenas      ; Minutos dezena
+
+    ; Display 0 - Segundo unidade (PD0)
+    mov reg_temp, r22
+    out PORT_BCD, reg_temp
+    ldi reg_temp, 0b00000001
+    out PORT_CTRL, reg_temp
+    rcall atraso_display
+
+    ; Display 1 - Segundo dezena (PD1)
+    mov reg_temp, r23
+    out PORT_BCD, reg_temp
+    ldi reg_temp, 0b00000010
+    out PORT_CTRL, reg_temp
+    rcall atraso_display
+
+    ; Display 2 - Minuto unidade (PD2)
+    mov reg_temp, r24
+    out PORT_BCD, reg_temp
+    ldi reg_temp, 0b00000100
+    out PORT_CTRL, reg_temp
+    rcall atraso_display
+
+    ; Display 3 - Minuto dezena (PD3)
+    mov reg_temp, r25
+    out PORT_BCD, reg_temp
+    ldi reg_temp, 0b00001000
+    out PORT_CTRL, reg_temp
+    rcall atraso_display
+
+    ; Apaga todos os displays ao final
+    clr reg_temp
+    out PORT_CTRL, reg_temp
+
+    pop r25
+    pop r24
+    pop r23
+    pop r22
+    pop reg_display
     pop reg_aux
     pop reg_unidades
     pop reg_dezenas
     pop reg_temp
     ret
 
-; ============ TIMERS ==================
+; ============ ATRASO ENTRE TROCAS DE DISPLAY ============
+atraso_display:
+    ldi reg_aux, 40           ; Laço externo reduzido
+loop_externo:
+    ldi reg_display, 50       ; Laço interno reduzido
+loop_interno:
+    dec reg_display
+    brne loop_interno
+    dec reg_aux
+    brne loop_externo
+    ret
+
+; ============ TRATAMENTO DE OVERFLOW ====================
 trata_overflow:
     push reg_temp
     push reg_dezenas
@@ -154,7 +207,6 @@ salva_min:
 
 salva_seg:
     sts segundos, reg_dezenas
-    call atualiza_displays
 
 fim_overflow:
     pop reg_status
